320482df3c00291242c4b89ebe17d8d7272f0e25
Kinchin Fong <v@vvzero.com>
2025-03-20 11:18:49 +0800

    use waitress for WSGI

--- main.py
+++ main.py
@@ -54,4 +54,5 @@ def DDNS():
         return jsonify({'code': 0, 'msg': 'OK'}), 200
 
 if __name__ == '__main__':
-    app.run(host='::', port=8000)
+    from waitress import serve
+    serve(app, host="127.0.0.1", port=8000)
